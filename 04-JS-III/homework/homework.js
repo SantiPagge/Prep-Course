4e1f344c236ede8fb1c97ff3a05e8bf43728d29a
SantiPagge <santy.pagge@gmail.com>
2022-05-22 19:15:33 -0300

    JSIII 14

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -92,6 +92,7 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+
   return agregarNumeros(resultadosTest) / resultadosTest.length;
 }
 
@@ -100,6 +101,12 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var resultado = numeros[0];
+  for (var i = 0; i < numeros.length; i++){
+    if(numeros[i] > resultado){
+      resultado = numeros[i];
+    }
+  } return resultado;
 }
 
 
@@ -107,13 +114,23 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length < 1) return 0;
+  var total = 1;
+  for (var i = 0; i < arguments.length; i++) {
+    total = total * arguments[i]
+  }return total;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var total = 0
+  for(var i = 0; i<arreglo.length; i++){
+if(arreglo[i]>18){
+  total++
+}
+  } return total;
 }
 
 
@@ -122,7 +139,11 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+
+    if(numeroDeDia === 1 || numeroDeDia === 7 ){
+    return "Es fin de semana"
+  }
+  return "Es dia Laboral"
 } 
 
 
@@ -130,7 +151,6 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
 }
 
 
