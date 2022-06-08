22bb7e69a608237c491a1c57842a29517f0891a3
SantiPagge <santy.pagge@gmail.com>
2022-06-08 16:16:37 -0300

    JSIV Completo

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -141,6 +141,11 @@ function sumarLikesDeUsuario (usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var resultado = 0;
+  for (var i = 0; i < usuario.posts.length; i++){
+    resultado = resultado + usuario.posts[i].likes;
+  }
+  return resultado;
 }
 
 function agregarMetodoCalculoDescuento (producto) {
@@ -153,7 +158,10 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = function() {
+    return this.precio - ( this.precio * this.porcentajeDeDescuento );
+  };
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
