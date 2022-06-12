690b59db5495ccaccb5bc96883ddb8769c19b989
SantiPagge <santy.pagge@gmail.com>
2022-06-11 21:02:14 -0300

    JSV Completo

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -37,12 +37,12 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-  String.prototype.reverse = function(){
-    var string = "";
-    for (var i = this.length - 1; i>0; i--){
-      string = string + this.charAt(i);
+  String.prototype.reverse = function() {
+    var stringInvertida = "";
+    for(var i = this.length - 1; i>=0; i--) {
+      stringInvertida = stringInvertida + this.charAt(i);
     }
-    return string;
+    return stringInvertida;
   };
 }
 
@@ -58,12 +58,12 @@ function agregarStringInvertida() {
     //  }
 
   class Persona {
-    constructor(Nombre, Apellido, Edad, Domicilio)/*Escribir los argumentos que recibe el constructor*/{
+    constructor(nombre, apellido, edad, domicilio)/*Escribir los argumentos que recibe el constructor*/{
       // Crea el constructor:
-      this.Nombre = nombre;
-      this.Apellido = apellido;
-      this.Edad = edad;
-      this.Domicilio = domicilio;
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
       this.detalle = function(){
         return {
         Nombre: this.nombre,
@@ -79,11 +79,16 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  var persona = new Persona(nombre, apellido, edad, dir);
+  return persona;
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  Persona.prototype.datos = function(){
+    return this.nombre + ", " + this.edad + " años";
+  }
 }
   
 
